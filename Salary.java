bb94c9e758d632aaee2c9cb401051bc96ba77da5
Gabriela Salagean <gaby.slg@icloud.com>
2025-02-28 10:48:03 -0500

    salary

--- Salary.java
+++ Salary.java
@@ -11,17 +11,6 @@
 //3 - print net pay
 public class Salary
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Salary
-     */
-    public Salary()
-    {
-        // initialise instance variables
-        x = 0;
-    }
 
     /**
      * An example of a method - replace this comment with your own
@@ -29,9 +18,12 @@ public class Salary
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void getSalary (int workedHours, double wage){
+        double taxes = 0.30;
+        double grossPay = workedHours * wage;
+        double netPay = grossPay - (grossPay * taxes);
+        System.out.println(netPay);
     }
-}
+    
+    }
+
